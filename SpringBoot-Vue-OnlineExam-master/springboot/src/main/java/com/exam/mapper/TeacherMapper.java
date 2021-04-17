2c728d379190727ec2077d0a2a57c1563ed88a55
lishi-1 <58899297+lishi-1@users.noreply.github.com>
2021-04-17 14:02:43 +0800

    基本功能
    Signed-off-by: lishi-1 <58899297+lishi-1@users.noreply.github.com>

---
+++ SpringBoot-Vue-OnlineExam-master/springboot/src/main/java/com/exam/mapper/TeacherMapper.java
@@ -0,0 +1,36 @@
+package com.exam.mapper;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.exam.entity.Teacher;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface TeacherMapper {
+
+    @Select("select * from teacher")
+    IPage<Teacher>  findAllByPage(Page page);
+
+    @Select("select * from teacher")
+    public List<Teacher> findAll();
+
+    @Select("select * from teacher where teacherId = #{teacherId}")
+    public Teacher findById(Integer teacherId);
+
+    @Delete("delete from teacher where teacherId = #{teacherId}")
+    public int deleteById(Integer teacherId);
+
+    @Update("update teacher set teacherName = #{teacherName},sex = #{sex}," +
+            "tel = #{tel}, email = #{email},pwd = #{pwd},cardId = #{cardId}," +
+            "role = #{role},institute = #{institute},type = #{type} where teacherId = #{teacherId}")
+    public int update(Teacher teacher);
+
+    @Options(useGeneratedKeys = true,keyProperty = "teacherId")
+    @Insert("insert into teacher(teacherName,sex,tel,email,pwd,cardId,role,type,institute) " +
+            "values(#{teacherName},#{sex},#{tel},#{email},#{pwd},#{cardId},#{role},#{type},#{institute})")
+    public int add(Teacher teacher);
+}
